d5cd9b2ee7a30c86d47cb22c45fba9f7398da492
Big Al <alejandro@osorio.pro>
2018-01-16 16:19:45 -0300

    Version Final

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,37 @@
-##PROBANDO ACTUALIZACION EN GITHUB - a ver si sube directo
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix codes a set of functions and specifies a couple of variables
+## within its parent environment.  That allows the next function to use cached results
 
 makeCacheMatrix <- function(x = matrix()) {
+
+        inv <- matrix(NA,nrow(x),ncol(x))
+        set <- function(y) {
+                x <<- y
+                inv <<- matrix(NA,nrow(x),ncol(x))
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calculates the inverse of the matrix specified in the former function
+## unless that result has been calculated and stored as cached data before
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+        inv <- x$getinverse()
+        if(all(!is.na(inv))) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
+
 }
